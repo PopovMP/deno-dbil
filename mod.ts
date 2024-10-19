5ccceeacfd4884364b5707b5e31e9303c65aaeca
Miroslav Popov <info@forexsb.com>
2024-10-19 22:11:29 +0300

    Added example

--- mod.ts
+++ mod.ts
@@ -1,2 +1,38 @@
+/**
+ * Symple, embedded, in-memory, document-oriented database
+ *
+ * Syntax similar to MongoDB.
+ *
+ * Exmaple:
+ *
+ * ```typescript
+ * import { getDb } from "@popov/dbil";
+ *
+ * const dirname = Deno.cwd() + "/db";
+ * const db = getDb({ name: "test", dirname });
+ *
+ * db.insert({ name: "John", age: 25 });
+ * db.insert({ name: "Allice", age: 19 });
+ *
+ * const docs = db.find({ age: {$gt: 20} });
+ *
+ * console.log(doc.length); // 1
+ * console.log(doc[0].name); // "John"
+ *
+ * db.update({ name: "Allice" }, { $set: { age: 21 } });
+ *
+ * const doc = db.findOne({ name: "Allice" }, { name: 1, age: 1 });
+ * console.log(doc.age); // 21
+ *
+ * const count = db.count({ age: {$gt: 20} });
+ * console.log(count); // 2
+ *
+ * const countRemoved = db.remove({ name: "Allice" });
+ * console.log(countRemoved); // 1
+ * ```
+ *
+ * @module dbil
+ */
+
 export * from "./dbil.d.ts";
 export * from "./dbil.ts";
