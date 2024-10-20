1b7581ca3734bdf136d9d2d9277adbd26194366d
Miroslav Popov <info@forexsb.com>
2024-10-20 16:20:23 +0300

    Added dbInsert test

---
+++ insert_test.ts
@@ -0,0 +1,45 @@
+import { assertEquals } from "@std/assert";
+
+import { dbInsert } from "./insert.ts";
+import type { DocMap } from "./dbil.d.ts";
+
+function getDocMap(): DocMap {
+  return {
+    "1": { _id: "1", val: 1 },
+    "2": { _id: "2", val: 2 },
+    "3": { _id: "3", val: 3 },
+  } as DocMap;
+}
+
+Deno.test("dbInsert inserts a doc", () => {
+  const docMap: DocMap = getDocMap();
+
+  const id: string = dbInsert(docMap, { val: 4 });
+  const doc = docMap[id];
+
+  assertEquals(doc.val, 4);
+});
+
+Deno.test("dbInsert creates a proper _id", () => {
+  const docMap: DocMap = getDocMap();
+
+  const id: string = dbInsert(docMap, {});
+
+  assertEquals(id.length, 16);
+});
+
+Deno.test("dbInsert inserts a doc with _id", () => {
+  const docMap: DocMap = getDocMap();
+
+  const id: string = dbInsert(docMap, { _id: "4", val: 4 });
+
+  assertEquals(id, "4");
+});
+
+Deno.test("dbInsert cannot insert a doc with existing _id", () => {
+  const docMap: DocMap = getDocMap();
+
+  const id: string = dbInsert(docMap, { _id: "1", val: 4 });
+
+  assertEquals(id, "");
+});
