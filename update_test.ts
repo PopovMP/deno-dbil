31d929a35e28c7ae76d738e3149e1aed129491fc
Miroslav Popov <info@forexsb.com>
2024-10-20 20:53:59 +0300

    Cleanup

--- update_test.ts
+++ update_test.ts
@@ -5,42 +5,42 @@ import type { Doc } from "./dbil.d.ts";
 
 Deno.test("dbUpdate $inc", () => {
   const doc: Doc = { _id: "1", val: 1 };
-  const numUpdated = dbUpdate(doc, { $inc: {val: 5} });
+  const numUpdated = dbUpdate(doc, { $inc: { val: 5 } });
   assertEquals(numUpdated, 1);
   assertEquals(doc.val, 6);
 });
 
 Deno.test("dbUpdate $push", () => {
   const doc: Doc = { _id: "1", vals: [1] };
-  const numUpdated = dbUpdate(doc, { $push: {vals: 5} });
+  const numUpdated = dbUpdate(doc, { $push: { vals: 5 } });
   assertEquals(numUpdated, 1);
   assertEquals(doc.vals, [1, 5]);
 });
 
 Deno.test("dbUpdate $rename", () => {
   const doc: Doc = { _id: "1", val: 1 };
-  const numUpdated = dbUpdate(doc, { $rename: {val: "count"} });
+  const numUpdated = dbUpdate(doc, { $rename: { val: "count" } });
   assertEquals(numUpdated, 1);
   assertEquals(doc.count, 1);
 });
 
 Deno.test("dbUpdate $set", () => {
   const doc: Doc = { _id: "1", val: 1 };
-  const numUpdated = dbUpdate(doc, { $set: {val: 2} });
+  const numUpdated = dbUpdate(doc, { $set: { val: 2 } });
   assertEquals(numUpdated, 1);
   assertEquals(doc.val, 2);
 });
 
 Deno.test("dbUpdate $unset 1", () => {
   const doc: Doc = { _id: "1", val: 1 };
-  const numUpdated = dbUpdate(doc, { $unset: {val: 1} });
+  const numUpdated = dbUpdate(doc, { $unset: { val: 1 } });
   assertEquals(numUpdated, 1);
   assertEquals(doc.val, undefined);
 });
 
 Deno.test("dbUpdate $unset 0", () => {
   const doc: Doc = { _id: "1", val: 1 };
-  const numUpdated = dbUpdate(doc, { $unset: {val: 0} });
+  const numUpdated = dbUpdate(doc, { $unset: { val: 0 } });
   assertEquals(numUpdated, 0);
   assertEquals(doc.val, 1);
 });
